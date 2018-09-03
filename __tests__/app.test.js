e065a451766c65dde214786e7de7c8f2e624f7ec
Soul Wong <soul_wong@dialog.com.au>
2018-09-03 15:00:25 +0800

    web server can tell jokes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,21 +3,27 @@ var request = require("supertest");
 describe("loading express", function() {
   var server;
 
-  beforeEach(function() {
-    server = require("../src/app")
+  beforeEach(() => {
+    server = require("../src/app");
   });
 
-  afterEach(function() {
+  afterEach(() => {
     server.close();
   });
 
-  it("responds to /", function testSlash(done) {
+  it("responds to /", done => {
     request(server)
       .get("/")
       .expect(200, done);
   });
 
-  it("404 everything else", function testPath(done) {
+  it("tells a joke to /jokes", done => {
+    request(server)
+      .get("/jokes")
+      .expect("The original joke", done);
+  });
+
+  it("404 everything else", done => {
     request(server)
       .get("/foo/bar")
       .expect(404, done);
--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.status(200).send("ok");
 });
 
-var server = app.listen(3000, function() {
+app.get("/jokes", (req, res) => {
+  res.status(200).send("The original joke");
+});
+
+var server = app.listen(3000, () => {
   var port = server.address().port;
   console.log("Example app listening at port %s", port);
 });
